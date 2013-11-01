45d486b340e94fd87a740631bc15acd41bb21ccf
Raymond T. Hightower <rth@wisdomgroup.com>
2013-10-31 19:21:04 -0500

    fill in the about page

--- build/MacOSX-10.8-Development/objs/init.mm
+++ build/MacOSX-10.8-Development/objs/init.mm
@@ -11,7 +11,7 @@ extern "C" {
     void rb_define_global_const(const char *, void *);
     void rb_rb2oc_exc_handler(void);
     void rb_exit(int);
-void MREP_A3491D3C70F2429D8FFC68287E032766(void *, void *);
+void MREP_7935C47A7EB3407AAA78C566793EBCE0(void *, void *);
 }
 
 extern "C"
@@ -32,7 +32,7 @@ RubyMotionInit(int argc, char **argv)
 	    void *self = rb_vm_top_self();
 rb_define_global_const("RUBYMOTION_ENV", @"development");
 rb_define_global_const("RUBYMOTION_VERSION", @"2.11");
-MREP_A3491D3C70F2429D8FFC68287E032766(self, 0);
+MREP_7935C47A7EB3407AAA78C566793EBCE0(self, 0);
 #if !__LP64__
 	}
 	catch (...) {
