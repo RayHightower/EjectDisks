ec8c410ad53928e0e13ba1a14c547fae39340220
Raymond T. Hightower <rth@wisdomgroup.com>
2013-10-31 19:41:16 -0500

    add a .gitignore file

--- build/MacOSX-10.8-Development/objs/init.mm
+++ build/MacOSX-10.8-Development/objs/init.mm
@@ -11,7 +11,7 @@ extern "C" {
     void rb_define_global_const(const char *, void *);
     void rb_rb2oc_exc_handler(void);
     void rb_exit(int);
-void MREP_BD99CAA917D74CCD99A3518CC344D886(void *, void *);
+void MREP_30B1306729F44311B39FFD6BFE07B612(void *, void *);
 }
 
 extern "C"
@@ -32,7 +32,7 @@ RubyMotionInit(int argc, char **argv)
 	    void *self = rb_vm_top_self();
 rb_define_global_const("RUBYMOTION_ENV", @"development");
 rb_define_global_const("RUBYMOTION_VERSION", @"2.11");
-MREP_BD99CAA917D74CCD99A3518CC344D886(self, 0);
+MREP_30B1306729F44311B39FFD6BFE07B612(self, 0);
 #if !__LP64__
 	}
 	catch (...) {
